2f7aea6cffd9aa0fd133524162b9561a63f6c70a
MB-teacher <MB-teacher@MB-207-teacher>
2019-12-28 12:03:42 +0800

    arduino

--- arduino_code/Blink2/Blink2.ino
+++ arduino_code/Blink2/Blink2.ino
@@ -1,13 +1,16 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(50);                       // wait for a second
+  digitalWrite(13, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
   digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
-  delay(50);                       // wait for a second
+  digitalWrite(13, HIGH);    // turn the LED off by making the voltage LOW
+  delay(100);                       // wait for a second
   
 }
