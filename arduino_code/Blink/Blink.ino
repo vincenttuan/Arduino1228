c6258b2a97ed44417877c27deee0740629a6a495
MB-teacher <MB-teacher@MB-207-teacher>
2019-12-28 11:35:55 +0800

    arduino

--- arduino_code/Blink/Blink.ino
+++ arduino_code/Blink/Blink.ino
@@ -6,8 +6,8 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(300);                       // wait for a second
+  delay(50);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(300);                       // wait for a second
+  delay(50);                       // wait for a second
 
 }
