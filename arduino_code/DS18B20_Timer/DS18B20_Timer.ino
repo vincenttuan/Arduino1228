b3365bf8b7555e3c3f17444da80e793b9f9bce6c
MB-teacher <MB-teacher@MB-207-teacher>
2020-01-18 10:43:53 +0800

    arduino

---
+++ arduino_code/DS18B20_Timer/DS18B20_Timer.ino
@@ -0,0 +1,21 @@
+#include <Timer.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define pin 7
+
+OneWire ds(pin); // 初始化 ds 物件
+DallasTemperature DS18B20(&ds); // 初始化 DS18B20 物件
+
+Timer t; // 宣告 Timer 物件
+
+void setup() {
+  Serial.begin(9600);
+  DS18B20.begin(); // DS18B20 啟動
+}
+
+void loop() {
+  DS18B20.requestTemperatures(); // DS18B20 開始偵測環境溫度
+  float data = DS18B20.getTempCByIndex(0); // 取得溫度 ( 攝氏 --> TempC )
+  Serial.println(data, 2); // 溫度取到小數點第 2 位
+  delay(100);
+}
