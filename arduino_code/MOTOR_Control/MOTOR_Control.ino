4679da5ee83404736547bfad95d6ec6672eeb53f
MB-teacher <MB-teacher@MB-207-teacher>
2020-02-01 11:26:24 +0800

    arduino

--- arduino_code/MOTOR_Control/MOTOR_Control.ino
+++ arduino_code/MOTOR_Control/MOTOR_Control.ino
@@ -1,11 +1,12 @@
+#include <Timer.h>
 #define M1_pin 5 // Rifgt ENB
 #define M2_pin 6 // Left ENA
 #define L1_pin 12 // IN1 F
 #define L2_pin 11 // IN2 B
 #define R1_pin 10  // IN3 F
 #define R2_pin 9 // IN4 B
-
-#define POWER_LED_PIN 13
+#define BUZZER_pin 8
+#define POWER_LED_pin 13
 
 boolean forward = true;
 boolean power_enabled = false;
@@ -13,22 +14,31 @@ int default_power = 50;
 int right_power = default_power;
 int light_power = default_power;
 
+Timer t; // 宣告 Timer 物件
+
 void setup() {
   Serial.begin(9600);
-  pinMode(POWER_LED_PIN, OUTPUT);
+  pinMode(POWER_LED_pin, OUTPUT);
   pinMode(L1_pin, OUTPUT);
   pinMode(L2_pin, OUTPUT);
   pinMode(R1_pin, OUTPUT);
   pinMode(R2_pin, OUTPUT);
+  pinMode(BUZZER_pin, OUTPUT);
+  t.every(0, car);
+  t.every(0, backBuzeer);
 }
 
 void loop() {
+  t.update();
+}
+
+void car() {
   char c = Serial.read();
   switch (c) {
     case '1':
       power_enabled = true;
       forward = true;
-      digitalWrite(POWER_LED_PIN, HIGH);
+      digitalWrite(POWER_LED_pin, HIGH);
       analogWrite(M1_pin, default_power);
       analogWrite(M2_pin, default_power);
       right_power = default_power;
@@ -37,7 +47,7 @@ void loop() {
       break;
     case '0':
       power_enabled = false;
-      digitalWrite(POWER_LED_PIN, LOW);
+      digitalWrite(POWER_LED_pin, LOW);
       analogWrite(M1_pin, 0);
       analogWrite(M2_pin, 0);
       printPower();
@@ -61,22 +71,36 @@ void loop() {
       forward = true;
       wheelControl();
       printPower();
-      break;  
+      break;
     case 'b':
       if (!power_enabled) break;
       forward = false;
       wheelControl();
       printPower();
-      break;  
+      break;
     case 't':
       if (!power_enabled) break;
       right_power = 255;
       light_power = 255;
       wheelControl();
       printPower();
-      break;      
+      break;
   }
-  
+
+}
+
+void backBuzeer() {
+  if (!power_enabled) return;
+  if(forward) {
+    digitalWrite(POWER_LED_pin, HIGH);
+    return;
+  }
+  digitalWrite(BUZZER_pin, HIGH);
+  digitalWrite(POWER_LED_pin, HIGH);
+  delay(1000);
+  digitalWrite(BUZZER_pin, LOW);
+  digitalWrite(POWER_LED_pin, LOW);
+  delay(1000);
 }
 
 int addPower(int power) {
@@ -103,7 +127,7 @@ void wheelControl() {
     digitalWrite(L1_pin, LOW);
     digitalWrite(L2_pin, HIGH);
     digitalWrite(R1_pin, LOW);
-    digitalWrite(R2_pin, HIGH);  
+    digitalWrite(R2_pin, HIGH);
   }
 }
 
