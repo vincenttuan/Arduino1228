2b8d2c2d0b9b7c1a86ccfa38f42abef4a8711974
MB-teacher <MB-teacher@MB-207-teacher>
2020-02-08 14:46:21 +0800

    arduino

---
+++ arduino_code/BT_Control_PWMLED/BT_Control_PWMLED.ino
@@ -0,0 +1,19 @@
+// 使用手機控制燈光 
+// 手機請在 Google Play 下載 Arduino bluetooth controller APP
+#include  <SoftwareSerial.h>
+#define LED_PIN 9
+SoftwareSerial BTSerial(11, 10); // BT: RX->11 | TX->10
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT) ;
+  Serial.begin(9600); // 57600 或 9600  
+  BTSerial.begin(9600); // 57600 或 9600  
+}
+
+void loop() {
+  if (BTSerial.available()) {
+    int i = BTSerial.read();
+    analogWrite(LED_PIN, i);
+    Serial.println(i);
+  }
+}
