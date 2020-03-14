3349ed486417fc2b2160c9cd938ec4ecda910616
MB-teacher <MB-teacher@MB-207-teacher>
2020-03-14 12:17:34 +0800

    arduino

--- arduino_code/Java_RFID/Java_RFID.ino
+++ arduino_code/Java_RFID/Java_RFID.ino
@@ -1,3 +1,4 @@
+#include <Timer.h>
 #include <SPI.h>
 #include <RFID.h>
 #define RST_PIN 9
@@ -5,6 +6,9 @@
 #define BUZZER_PIN 8
 
 RFID rfid(SS_PIN, RST_PIN);
+unsigned char keyA[16] {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xff, 0x07, 0x80, 0x69, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+int key_blockAddr = 11; // 資料驗證區 Data block 11 中的密碼 A
+int data_blockAddr = 8; // 資料撰寫區 Data block 8
 
 void setup() {
   Serial.begin(9600);
